83c42fc59f901182763510b2f01b5eca6bb6a24e
Jayden <gwanggi.lee@gmail.com>
2019-03-06 23:55:24 +0900

    update blog meta config

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/jayden-lee/jayden-lee.tech"
       className="github"
       aria-label="GitHub"
     >
