d3feb5903320dd6a8367d13f9f98838c4a6a923a
Jayden <gwanggi.lee@gmail.com>
2019-03-09 15:23:54 +0900

    Update blog meta config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,27 +3,33 @@ module.exports = {
   description: `Blog posted about development.`,
   author: `Jayden`,
   introduction: `소프트웨어 엔지니어: 주로 자바 언어를 이용하여 개발을 했으며, 최근에는 코틀린 언어에 관심을 갖고 있습니다.`,
-  siteUrl: `https://jayden-lee.tech`, // Your blog site url
+  siteUrl: `https://jayden-lee.tech`,
   social: {
-    twitter: ``, // Your Twitter account
-    github: `jayden-lee`, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    twitter: ``,
+    github: `jayden-lee`,
+    medium: ``,
+    facebook: `gglee0127`,
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [
+    `blog`,
+    `java`,
+    `kotlin`,
+    `database`,
+    `dev`
+  ],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'jayden-lee/jayden-lee.tech', // Your repository for archive comment
+    disqusShortName: '',
+    utterances: 'jayden-lee/jayden-lee.tech',
   },
   configs: {
-    countOfInitialPost: 10, // Config your initial count of post
+    countOfInitialPost: 10,
   },
   sponsor: {
     buyMeACoffeeId: 'jaydenlee',
   },
   share: {
-    facebookAppId: '2355673971344620', // Add facebookAppId for using facebook share feature v3.2
+    facebookAppId: '2355673971344620',
   },
-  ga: 'UA-129022417-2', // Add your google analytics tranking ID
+  ga: 'UA-129022417-2',
 }
